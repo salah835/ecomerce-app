9888bf70b2fa1c1a1ef0412d6d217dae081b428f
salah835 <salahmba004@gmail.com>
2026-02-09 16:59:26 +0100

    fixe

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Navbar from "./components/NavbAR.JSX";
+import Navbar from "./components/Navbar.JSX";
 import Sidebar from "./components/Sidebar";
 import { Routes, Route } from "react-router-dom";
 import Add from "./pages/Add";
